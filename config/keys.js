fe270f77347e3ea791bdcb6db17595c175fc23a1
Victor Park <09pve09@gmail.com>
2017-11-06 23:08:19 -0800

    finished auth flow(connected mongoDB,applied cookie-session)

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//keys
+
+if(process.env.NODE_ENV === 'production'){
+//we re in production
+  module.exports = require('./prod');
+}else{
+//we re in development
+  module.exports = require('./dev');
+}
